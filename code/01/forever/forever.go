7a928b9b6b9346678d870900a8fb376fd575f1c1
Luciano Ramalho <luciano@ramalho.org>
2018-03-19 19:01:24 -0300

    working on forever.go exercise

--- code/01/forever/forever.go
+++ code/01/forever/forever.go
@@ -5,10 +5,25 @@ import (
 	"time"
 )
 
+const (
+	timeFormat = "1/2 3:04:05 PM"
+	remoteZone = "Pacific/Fiji"
+	delay      = 1 * time.Second
+)
+
 func main() {
+	remoteLoc, err := time.LoadLocation(remoteZone)
+	if err != nil {
+		panic(err)
+	}
 	for {
-		now := time.Now().In(time.Local)
-		fmt.Println(now.Format("3:04:05 PM"))
-		time.Sleep(1 * time.Second)
+		now := time.Now()
+		utc := now.In(time.UTC)
+		local := now.In(time.Local)
+		remote := now.In(remoteLoc)
+		fmt.Print("UTC ", utc.Format(timeFormat), " | ")
+		fmt.Print("Local ", local.Format(timeFormat), " | ")
+		fmt.Println(remoteZone, remote.Format(timeFormat))
+		time.Sleep(delay)
 	}
 }
